4fb544b3ec9a2ef20b1a1063e7af75cf8b8b5608
우경훈 <khwoo@sangsang.farm>
2024-10-14 14:45:30 +0900

    feat: ESLint, Prettier 설정 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,62 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+    root: true,
+    extends: [
+        '@react-native',
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:jsx-a11y/recommended',
+    ],
+    plugins: [
+        '@typescript-eslint',
+        'react',
+        'react-hooks',
+        'jsx-a11y',
+        'import',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2021,
+        sourceType: 'module',
+    },
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+        'react-native/react-native': true,
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    rules: {
+        '@typescript-eslint/no-unused-vars': [
+            'error',
+            {argsIgnorePattern: '^_'},
+        ],
+        'react/prop-types': 'off',
+        'react/jsx-filename-extension': [1, {extensions: ['.tsx']}],
+        'import/order': [
+            'error',
+            {
+                groups: [
+                    'builtin',
+                    'external',
+                    'internal',
+                    'parent',
+                    'sibling',
+                    'index',
+                ],
+                'newlines-between': 'always',
+            },
+        ],
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+        'jsx-a11y/accessible-emoji': 'warn',
+    },
 };
