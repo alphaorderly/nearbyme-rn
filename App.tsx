66c0d966de7917d3ed06aab1119e42399a3f87b8
우경훈 <khwoo@sangsang.farm>
2024-10-14 15:07:08 +0900

    feat: react navigation 설치

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
+import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
 
 function App(): React.JSX.Element {
-  return (
-    <SafeAreaView className="flex-1">
-      <View className="flex-1 justify-center items-center">
-        <Text className="text-2xl font-bold">Hello World</Text>
-      </View>
-    </SafeAreaView>
-  );
+    return (
+        <NavigationContainer>
+            <SafeAreaView className="flex-1">
+                <View className="flex-1 justify-center items-center">
+                    <Text className="text-2xl font-bold">Hello World</Text>
+                </View>
+            </SafeAreaView>
+        </NavigationContainer>
+    );
 }
 
 export default App;
